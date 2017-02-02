467af9d3b12f2d82818b6461b2a684eb5f486995
Calibash <wanderingvagrant@hotmail.com>
2017-02-01 23:32:58 -0600

    changed CatLogic class to more general PetLogic, and added additional information on intent

---
+++ Cats-in-dapper-hats/src/cats/in/dapper/hats/PetLogic.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cats.in.dapper.hats;
+
+/**
+ *provide the logical behavior of a "pet" in the game. 
+ * 
+ * the original idea was something along the lines of having a bunch of floating
+ * cat/bunny/puppy/etc heads that made homes in hats. Play on words and all 
+ * that, and rather than fully grown cats, they'd be juvenile. 
+ * playing up the cute angle.
+ * 
+ * The development of the idea brought up the possibility of the cats engaging 
+ * in different sorts of behaviors. specifically, interacting with each other 
+ * as well as potentially the user via interest, affection, curiosity, dislike, 
+ * and so on.
+ * 
+ * preliminary expressed emotions;
+ * interest;
+ * affection;
+ * dislike;
+ * curiosity;
+ * happiness;
+ * sadness;
+ * 
+ * each instantiated pet unit should also have a slightly different personality,
+ * expressing more of some trait and less of others, or veering towards specific
+ * preprogrammed or generated behavioral patterns. pattern composition should be
+ * used to provide an almost endless variation matrix.
+ * @author Calibash
+ */
+public class PetLogic {
+    
+    
+    
+}

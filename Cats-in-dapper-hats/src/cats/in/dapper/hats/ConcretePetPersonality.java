620ade97cb723bcb0501a1aaba74838314907fbc
Calibash <wanderingvagrant@hotmail.com>
2017-02-07 23:26:39 -0600

    added concrete base pet personality class

---
+++ Cats-in-dapper-hats/src/cats/in/dapper/hats/ConcretePetPersonality.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cats.in.dapper.hats;
+
+/**
+ *
+ * @author Calibash
+ */
+public class ConcretePetPersonality implements IPetPersonality{
+    public ConcretePetPersonality(
+            Integer curiousityValue, 
+            Integer CheerfullnessValue,
+            Integer StubbornnessValue,
+            Integer SadnessValue){};
+    @Override
+    public void experienceEvent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer curiosityTendency() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer cheerfulnessTendency() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer stubbornnessTendency() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer sadnessTendency() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

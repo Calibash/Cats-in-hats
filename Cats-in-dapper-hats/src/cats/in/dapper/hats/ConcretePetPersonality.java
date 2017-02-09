16242d2dd901cadfbd6ed73c4315ee15da20443b
Calibash <wanderingvagrant@hotmail.com>
2017-02-08 23:52:08 -0600

    added tests for concrete pet personality. currently unsupported

--- Cats-in-dapper-hats/src/cats/in/dapper/hats/ConcretePetPersonality.java
+++ Cats-in-dapper-hats/src/cats/in/dapper/hats/ConcretePetPersonality.java
@@ -10,14 +10,24 @@ package cats.in.dapper.hats;
  * @author Calibash
  */
 public class ConcretePetPersonality implements IPetPersonality{
+    private Integer curiousityLevel;
+    private Integer cheerfulnessLevel;
+    private Integer stubbornnessLevel;
+    private Integer sadnessLevel;
     public ConcretePetPersonality(
             Integer curiousityValue, 
-            Integer CheerfullnessValue,
-            Integer StubbornnessValue,
-            Integer SadnessValue){};
+            Integer cheerfullnessValue,
+            Integer stubbornnessValue,
+            Integer sadnessValue){
+        this.curiousityLevel = curiousityValue;
+        this.cheerfulnessLevel = curiousityValue;
+        this.stubbornnessLevel = stubbornnessValue;
+        this.sadnessLevel = sadnessValue;
+    };
     @Override
     public void experienceEvent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("an event just happened");
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
---
+++ Cats-in-dapper-hats/test/cats/in/dapper/hats/ConcretePetPersonalityTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cats.in.dapper.hats;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Calibash
+ */
+public class ConcretePetPersonalityTest {
+    
+    public ConcretePetPersonalityTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of experienceEvent method, of class ConcretePetPersonality.
+     */
+    @Test
+    public void testExperienceEvent() {
+        System.out.println("experienceEvent");
+        ConcretePetPersonality instance = new ConcretePetPersonality(1,1,1,1);
+        instance.experienceEvent();
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of curiosityTendency method, of class ConcretePetPersonality.
+     */
+//    @Test
+    public void testCuriosityTendency() {
+        System.out.println("curiosityTendency");
+        ConcretePetPersonality instance = new ConcretePetPersonality(1,1,1,1);
+        Integer expResult = null;
+        Integer result = instance.curiosityTendency();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of cheerfulnessTendency method, of class ConcretePetPersonality.
+     */
+//    @Test
+    public void testCheerfulnessTendency() {
+        System.out.println("cheerfulnessTendency");
+        ConcretePetPersonality instance = new ConcretePetPersonality(1,1,1,1);
+        Integer expResult = null;
+        Integer result = instance.cheerfulnessTendency();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of stubbornnessTendency method, of class ConcretePetPersonality.
+     */
+//    @Test
+    public void testStubbornnessTendency() {
+        System.out.println("stubbornnessTendency");
+        ConcretePetPersonality instance = new ConcretePetPersonality(1,1,1,1);
+        Integer expResult = null;
+        Integer result = instance.stubbornnessTendency();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sadnessTendency method, of class ConcretePetPersonality.
+     */
+//    @Test
+    public void testSadnessTendency() {
+        System.out.println("sadnessTendency");
+        ConcretePetPersonality instance = new ConcretePetPersonality(1,1,1,1);
+        Integer expResult = null;
+        Integer result = instance.sadnessTendency();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}

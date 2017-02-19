2ca6e3703105236d888f9671f019cf5b3d8394ef
Calibash <wanderingvagrant@hotmail.com>
2017-02-18 23:41:57 -0600

    abstract outline for pet

---
+++ Cats-in-dapper-hats/src/cats/in/dapper/hats/AbstractPet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cats.in.dapper.hats;
+
+/**
+ *staging location for general pet interface interaction.
+ * @author Calibash
+ */
+public abstract class AbstractPet {
+    public AbstractPet(IPetPersonality petPersonality){
+        //include general interaction elements such as personality, logo, etc
+    }
+    
+    //something to output the pet object's visual image
+    
+    //something to allow user interaction with the pet object
+    
+    //specific exposure of pet personality interactions
+}
